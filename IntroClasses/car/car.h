935705f40ffd941eca9926444615c543233a182f
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2022-09-11 13:47:38 -0400

    Rename IntroClasses/car.h to IntroClasses/car/car.h

--- IntroClasses/car/car.h
+++ IntroClasses/car/car.h
@@ -1,17 +1,17 @@
 /*Write a class named Car that has the following member variables:
-• yearModel . An int that holds the car’s year model.
-• make . A string that holds the make of the car.
-• speed . An int that holds the car’s current speed.
+â€¢ yearModel . An int that holds the carâ€™s year model.
+â€¢ make . A string that holds the make of the car.
+â€¢ speed . An int that holds the carâ€™s current speed.
 
 In addition, the class should have the following constructor and other member functions.
-• Constructor. The constructor should accept the car’s year model and make as arguments.
-These values should be assigned to the object’s yearModel and make member
+â€¢ Constructor. The constructor should accept the carâ€™s year model and make as arguments.
+These values should be assigned to the objectâ€™s yearModel and make member
 variables. The constructor should also assign 0 to the speed member variables.
-• Accessor. Appropriate accessor functions to get the values stored in an object’s
+â€¢ Accessor. Appropriate accessor functions to get the values stored in an objectâ€™s
 yearModel , make , and speed member variables.
-• accelerate . The accelerate function should add 5 to the speed member variable
+â€¢ accelerate . The accelerate function should add 5 to the speed member variable
 each time it is called.
-• brake . The brake function should subtract 5 from the speed member variable each
+â€¢ brake . The brake function should subtract 5 from the speed member variable each
 time it is called.
 Demonstrate the class in a program that creates a Car object, and then calls the
 accelerate function five times. After each call to the accelerate function, get
