bd0eecfde33ec361f88f61c845b52b59c44120a0
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2022-09-12 13:27:19 -0400

    Update fileDisplayProgram.cpp

--- File Operations/fileDisplayProgram.cpp
+++ File Operations/fileDisplayProgram.cpp
@@ -1,8 +1,8 @@
 /*Write a program that asks the user for the name of a file.The program should display the
-contents of the file on the screen.If the file’s contents won’t fit on a single screen, the program
+contents of the file on the screen.If the fileâ€™s contents wonâ€™t fit on a single screen, the program
 should display 24 lines of output at a time, and then pause.Each time the program
 pauses, it should wait for the user to strike a key before the next 24 lines are displayed.*/
-/*
+
 #include<iostream>
 #include<fstream>
 #include<string>
@@ -39,4 +39,3 @@ int main()
 	return 0;
 
 }
-*/
