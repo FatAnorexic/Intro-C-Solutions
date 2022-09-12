502b836b05379a168ac10a725529c04b14020516
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2022-09-12 13:33:41 -0400

    Update tailprogram.cpp

--- File Operations/tailprogram.cpp
+++ File Operations/tailprogram.cpp
@@ -1,8 +1,8 @@
 /*Write a program that asks the user for the name of a file. The program should display
-the last 10 lines of the file on the screen (the ìtailî of the file). If the file has fewer than
+the last 10 lines of the file on the screen (the ‚Äútail‚Äù of the file). If the file has fewer than
 10 lines, the entire file should be displayed, with a message indicating the entire file
 has been displayed.*/
-/*
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -58,4 +58,3 @@ void displayTail(fstream& fin)
 		cout << ch;
 	}
 }
-*/
