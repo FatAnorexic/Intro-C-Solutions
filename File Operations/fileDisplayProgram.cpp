2ecc7e5b4a2b9b8778911d32f0980e839cf7c30a
FatAnorexic <alexbernhardt23@gmail.com>
2023-07-10 14:11:33 -0400

    Cleaned fileDisplayProgram.cpp may need refactor

--- File Operations/fileDisplayProgram.cpp
+++ File Operations/fileDisplayProgram.cpp
@@ -1,39 +1,40 @@
-/*Write a program that asks the user for the name of a file.The program should display the
-contents of the file on the screen.If the file’s contents won’t fit on a single screen, the program
-should display 24 lines of output at a time, and then pause.Each time the program
-pauses, it should wait for the user to strike a key before the next 24 lines are displayed.*/
+/*
+Write a program that asks the user for the name of a file.
+The program should display the contents of the file on 
+the screen. 
+
+If the file’s contents won’t fit on a single
+screen, the program should display 24 lines of output at a
+time, and then pause.Each time the program pauses, it 
+should wait for the user to strike a key before the next 
+24 lines are displayed.
+*/
 
 #include<iostream>
 #include<fstream>
 #include<string>
 
-using namespace std;
+int main(){
+	std::string filename, line;
+	std::fstream fin;
 
-int main()
-{
-	string filename, line;
-	
+	printf("Enter the name of the file you wish to open: ");
+	std::cin >> filename;
 
-	fstream fin;
-
-	cout << "Enter the name of the file you wish to open: ";
-	cin >> filename;
-
-	fin.open(filename, ios::in);
+	fin.open(filename, std::ios::in);
 	int count = 0;
 
-	while (getline(fin, line))
-	{
-		cout << line << endl;
-
+	while (getline(fin, line)){
+		std::cout << line << '\n';
 		count++;
-		if (count % 24 == 0)
-		{
-			cout << "\nPress enter key to continue.\n";
-			cin.get();
-			cin.ignore();
+
+		if (count % 24 == 0){
+			printf("\nPress enter key to continue.\n");
+			std::cin.get();
+			std::cin.ignore();
 		}
 	}
+	
 	fin.close();
 	system("pause");
 	return 0;
